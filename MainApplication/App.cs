b59c08e09ca407e6ede28a0ab25b854284a1f1d9
Elyes <dagdaguielyes50@gmail.com>
2026-05-29 17:04:07 +0100

    UPDATED Project Metadata

--- MainApplication/App.cs
+++ MainApplication/App.cs
@@ -1,6 +1,6 @@
 ﻿/// App.cs
 /// TSAAGPM(Techie's shader architecture and graphical processing model) >> 2D Basic shader computation model demo
-/// Project GitHub Repos : 
+/// Project GitHub Repos : https://github.com/elyes-dagdagui/TSAAGPM_C
 /// Developer Name       : ELYES DAGDAGUI
 /// Developer Email      : dagdaguielyes50@gmail.com
 /// Developer GitHub     : https://github.com/elyes-dagdagui
